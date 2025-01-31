ffafc7240989f0bbcb770103976eef62af81de25
divesh2022 <165630306+divesh2022@users.noreply.github.com>
2025-01-31 19:49:37 +0530

    Create update_course.sql

---
+++ update_course.sql
@@ -0,0 +1,46 @@
+-- this query updates the value of syllabus column in the course table in the database
+UPDATE Course 
+  SET Syllabus = 'Introduction to Programming, Basics of C++, Data Structures' 
+  WHERE Course_Code = 'CS101';
+UPDATE Course 
+  SET Syllabus = 'Basic Concepts of Thermodynamics, Thermodynamic Systems, Laws of Thermodynamics' 
+  WHERE Course_Code = 'ME102';
+UPDATE Course 
+  SET Syllabus = 'Circuit Elements, Ohm\s Law, Kirchhoff\s Laws, AC/DC Circuits' 
+  WHERE Course_Code = 'EE103';
+UPDATE Course 
+  SET Syllabus = 'Types of Structures, Analysis Techniques, Load Calculations' 
+  WHERE Course_Code = 'CE104';
+UPDATE Course 
+  SET Syllabus = 'Binary Systems, Logic Gates, Digital Circuits' 
+  WHERE Course_Code = 'EC105';
+UPDATE Course 
+  SET Syllabus = 'Database Models, SQL, Database Design, Transactions'
+  WHERE Course_Code = 'IT106';
+UPDATE Course 
+  SET Syllabus = 'Process Design, Fluid Mechanics, Chemical Reactions' 
+  WHERE Course_Code = 'CH107';
+UPDATE Course 
+  SET Syllabus = 'Fundamentals of Aerodynamics, Airfoil Design, Wind Tunnels' 
+  WHERE Course_Code = 'AE108';
+UPDATE Course 
+  SET Syllabus = 'Genetic Modification, Cloning, CRISPR Technology' 
+  WHERE Course_Code = 'BT109';
+UPDATE Course 
+  SET Syllabus = 'Metallurgical Processes, Heat Treatment, Metal Alloys' 
+  WHERE Course_Code = 'MS110';
+UPDATE Course 
+  SET Syllabus = 'Water Cycle, Resource Management, Pollution Control' 
+  WHERE Course_Code = 'EN111';
+UPDATE Course 
+  SET Syllabus = 'Medical Imaging Techniques, MRI, Ultrasound, X-rays' 
+  WHERE Course_Code = 'BM112';
+UPDATE Course 
+  SET Syllabus = 'Reservoir Characterization, Drilling Techniques, Fluid Flow' 
+  WHERE Course_Code = 'PE113';
+UPDATE Course 
+  SET Syllabus = 'Control Systems, Sensors, Actuators, Robotics Programming' 
+  WHERE Course_Code = 'RO114';
+UPDATE Course 
+  SET Syllabus = 'Automotive Design Principles, Engine Mechanics, Vehicle Dynamics' 
+  WHERE Course_Code = 'AU115';

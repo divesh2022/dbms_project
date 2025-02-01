41684cd2260a841c689be377bab75e7aaf93b290
divesh2022 <165630306+divesh2022@users.noreply.github.com>
2025-02-01 16:15:40 +0530

    Update and rename transaction.sql to transaction1.sql

--- transaction1.sql
+++ transaction1.sql
@@ -13,9 +13,21 @@ DECLARE @SGPA DECIMAL(4, 2);
 DECLARE @Projects VARCHAR(255);
 DECLARE @CareerPreference VARCHAR(255);
 
+-- Variables for transaction record
+DECLARE @TransactionTimestamp DATETIME;
+DECLARE @TransactionID VARCHAR(50);
+DECLARE @TransactionStatus VARCHAR(10);
+DECLARE @AdminName VARCHAR(100);
+
+-- Assigning values to transaction record
+SET @TransactionTimestamp = GETDATE();
+SET @TransactionID = NEWID();
+SET @TransactionStatus = 'success';
+SET @AdminName = 'fiona.white';
+
 -- Assigning values to variables
-SET @AdminLoginID = 'alice.johnson';
-SET @AdminPassword = 'pass1234';
+SET @AdminLoginID = 'fiona.white';
+SET @AdminPassword = 'itsupp12';
 SET @RollNo = 16;
 SET @Branch = 'Data Science';
 SET @Semester = 1;
@@ -34,8 +46,13 @@ BEGIN TRANSACTION;
 IF EXISTS (SELECT 1 FROM Admin WHERE Login_ID = @AdminLoginID AND Password = @AdminPassword)
 BEGIN
     -- Insert new student data
-    INSERT INTO Students (Roll_No, Branch, Semester, Subjects, Marks, Attendance, CGPA, SGPA, Projects, Career_Preference)
+    INSERT INTO Student (Roll_No, Branch, Semester, Subjects, Marks, Attendance, CGPA, SGPA, Projects, Career_Preference)
     VALUES (@RollNo, @Branch, @Semester, @Subjects, @Marks, @Attendance, @CGPA, @SGPA, @Projects, @CareerPreference);
+
+    -- Insert transaction record
+    INSERT INTO TransactionRecord (s_no, timestamp, transaction_id, status, admin)
+    VALUES (2, @TransactionTimestamp, @TransactionID, @TransactionStatus, @AdminName);
+
 
     -- Commit transaction
     COMMIT TRANSACTION;

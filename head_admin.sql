7a6ce440c526bc7d3fcbc7ce0dcd6fd771b8ca16
divesh2022 <165630306+divesh2022@users.noreply.github.com>
2025-02-01 10:05:09 +0530

    Create head_admin.sql

---
+++ head_admin.sql
@@ -0,0 +1,4 @@
+-- insert the data value for head admin
+INSERT INTO Admin (Admin_ID, Name, Role, Login_ID, Password)
+VALUES
+(1, 'Alice Johnson', 'System Administrator', 'alice.johnson', 'pass1234');

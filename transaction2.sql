5fd48b39614e9fecd0032029c9281e2ce6b3147e
divesh2022 <165630306+divesh2022@users.noreply.github.com>
2025-02-01 16:14:16 +0530

    Update transaction2.sql

--- transaction2.sql
+++ transaction2.sql
@@ -7,6 +7,18 @@ DECLARE @AdminPassword1 CHAR(8);
 SET @AdminLoginID1 = 'alice.johnson';
 SET @AdminPassword1 = 'pass1234';
 
+-- Variables for transaction record
+DECLARE @TransactionTimestamp DATETIME;
+DECLARE @TransactionID VARCHAR(50);
+DECLARE @TransactionStatus VARCHAR(10);
+DECLARE @AdminName VARCHAR(100);
+
+-- Assigning values to transaction record
+SET @TransactionTimestamp = GETDATE();
+SET @TransactionID = NEWID();
+SET @TransactionStatus = 'success';
+SET @AdminName = 'alice.johnson';
+
 -- Beginning transaction T1
 BEGIN TRANSACTION T1;
 
@@ -14,7 +26,12 @@ BEGIN TRANSACTION T1;
 IF EXISTS (SELECT 1 FROM Admin WHERE Login_ID = @AdminLoginID1 AND Password = @AdminPassword1)
 BEGIN
     -- Add new column
-    ALTER TABLE Students ADD Name VARCHAR(100);
+    ALTER TABLE Student ADD Name VARCHAR(100);
+
+        -- Insert transaction record
+    INSERT INTO TransactionRecord (s_no, timestamp, transaction_id, status, admin)
+    VALUES (2, @TransactionTimestamp, @TransactionID, @TransactionStatus, @AdminName);
+
 
     -- Commit transaction T1
     COMMIT TRANSACTION T1;
